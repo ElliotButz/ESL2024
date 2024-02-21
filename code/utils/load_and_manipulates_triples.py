5fa91582b966b25580f6f38dc724535e065ae6e1
unknown <elliot.butz@etu.umontpellier.fr>
2024-02-21 08:38:51 +0100

    Added tsv_from_csv()

--- code/utils/load_and_manipulates_triples.py
+++ code/utils/load_and_manipulates_triples.py
@@ -36,3 +36,21 @@ def triples_from_csv(path_to_csv, columns_to_use = c.Node.features.value):
     # Create a dataframe from the list of triples
     return pd.DataFrame(triples, columns=['subject', 'predicate', 'object'])
 
+
+def tsv_from_csv(output_path, path_to_csv, columns_to_use = c.Node.features.value):
+    """
+    Creates .tsv file of triples from a CSV.
+    Subjects are index items, predicates are column names from columns_to_use, objects are non-NaN values in columns.
+
+    Parameters:
+    - output_path (str): Filepath or buffer to the output TSV file.
+    - path_to_csv (str): Filepath or buffer to the input CSV file.
+    - columns_to_use (list): A list of the columns to consider.
+    
+    Returns:
+    - None in triple format.
+    """
+
+    triples_from_csv(path_to_csv,
+                     columns_to_use = columns_to_use).to_csv(path_or_buf=output_path,
+                                                             sep = '\t', index=False)
