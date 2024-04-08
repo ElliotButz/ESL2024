eca693b2216326d21dda69db204a9a670d417a56
ElliotButz <elliot.butz@free.fr>
2024-04-08 15:20:18 +0200

    Leaving notebooks because cuda refuses to play with them.

---
+++ code/train_complex_with_lin_sim.py
@@ -0,0 +1,126 @@
+print("\nImports...")
+
+import sys
+sys.path.append("/home/ebutz/ESL2024/code/utils")
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+tqdm.pandas()
+
+import random
+
+import torch
+import torch.optim as optim
+import torch_geometric
+from torch_geometric.nn import ComplEx
+from torch_geometric.data import Data
+from torch_geometric.transforms import RandomLinkSplit
+from torch_geometric.loader import DataLoader
+import torch.nn.functional as F
+
+from nxontology.imports import from_file
+import os
+
+import play_with_complex as pwc
+
+import wandb
+
+
+# ------------- Paths ------------- #
+
+# Iric
+mapped_iric_path = '/home/ebutz/ESL2024/data/mapped_iric.tsv'
+
+# Model to train :
+hidden_channels = 15
+batch_size = 4096
+epochs = 1000
+lin_factor = 1
+
+params_save_name = f"PARAMS_ComplEx_6_times_{hidden_channels}_HC_{epochs}_epochs_{batch_size}_BS_on_full_iric"
+model_parameters_path = "/home/ebutz/ESL2024/data/mapping_datasets_and_model_for_genes_to_phenotypes_iric/"+params_save_name
+
+# Ontology
+url = "/home/ebutz/ESL2024/data/go-basic.json.gz"
+
+# ------------- Cuda ------------- #
+
+print("\nCuda check...")
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Device: '{device}'")
+print("Could reach GPU :", torch.Tensor([0,1]).to(device).is_cuda)
+
+
+# ------------- Loading datas ------------- #
+
+print("\nLoading iric...")
+
+mapped_iric = pd.read_csv(mapped_iric_path, sep = '\t')
+
+GO_to_map = mapped_iric.set_index('object')['mapped_object'].to_dict()
+map_to_GO = {value: key for key, value in GO_to_map.items()}
+
+# Checking dict :
+looks_ok: bool = True
+print('Number of triples in graph :', len(list(mapped_iric['object'])))
+for i in range(len(list(mapped_iric['object']))):
+    if GO_to_map[mapped_iric['object'][i]]!=mapped_iric['mapped_object'][i]:
+        looks_ok = False
+print('GO - Mapping dicts looks ok :', looks_ok)
+
+# Making dict of alternatives tails :
+mapped_alt_tails = {}
+for index, row in mapped_iric.iterrows():
+    key = (row['mapped_subject'], row['mapped_predicate'])
+    if key not in mapped_alt_tails:
+        mapped_alt_tails[key] = set()
+    mapped_alt_tails[key].update(set(row['mapped_alt_tails']))
+for key, value in mapped_alt_tails.items():
+    mapped_alt_tails[key]=np.array(list(value))
+
+
+# ------------- Making datasets ------------- #
+
+print("\nMaking datasets...")
+# Edges index
+heads = list(mapped_iric['mapped_subject'])
+tails = list(mapped_iric['mapped_object'])
+edge_index = torch.tensor([heads,tails], dtype=torch.long)
+# edges attributes
+edge_attributes = torch.tensor(mapped_iric['mapped_predicate'])
+
+
+iric_pyg = Data(
+                num_nodes = len(set(mapped_iric['object']).union(set(mapped_iric['subject']))),
+                edge_index = edge_index,
+                edge_attr = edge_attributes
+                )
+
+print(iric_pyg)
+
+print("\nDataset looks valid :",iric_pyg.validate(raise_on_error=True))
+
+transform = RandomLinkSplit(
+                            num_val = 0.1,
+                            num_test = 0.1,
+                            is_undirected=False,
+                            add_negative_train_samples=False,
+                            )
+
+train_set, val_set, test_set = transform(iric_pyg)
+print('Dataset splits look valid (train, val, test):',train_set.validate(raise_on_error=True),
+                                                      val_set.validate(raise_on_error=True),
+                                                      test_set.validate(raise_on_error=True))
+
+# ------------- Loading ontology ------------- #
+
+print("\nLoading ontology...")
+nxo = from_file('/home/elliot/Documents/ESL2024/data/go-basic.json.gz')
+nxo.freeze()
+
+# ------------- train and evaluate ------------- #
+
+pwc
---
+++ code/utils/play_with_complex.py
@@ -0,0 +1,382 @@
+import pandas as pd
+import torch
+import torch_geometric
+import torch.optim as optim
+from torch_geometric.nn import ComplEx
+import nxontology as nxo
+import copy
+import wandb
+
+
+def train(loader, model, optimizer, device):
+    model.train()
+    total_loss = total_examples = 0
+    for head_index, rel_type, tail_index in loader:
+
+        head_index, rel_type, tail_index = head_index.to(device), rel_type.to(device), tail_index.to(device)
+        
+        optimizer.zero_grad()
+        loss = model.loss(head_index, rel_type, tail_index)
+        loss.backward()
+        optimizer.step()
+        total_loss += float(loss) * head_index.numel()
+        total_examples += head_index.numel()
+    return total_loss / total_examples
+
+
+def train_with_similarity_shrinkage(loader, model, optimizer, lin_term, ontology, device):
+
+    '''
+    Shrinks the weights modification operated by SGD.
+    Shrinkage is lesser if Lin term is high (close to 1)
+
+    lin_term should have its values between 0 and 1.
+    Requires model.loss() to return the false tail_index generated.
+    Requires lin_term's args to be head_index, rel_type, tail_index, false_tail_index, ontology.
+    '''
+    model.train()
+    total_loss = total_examples = 0
+    for head_index, rel_type, tail_index in loader:
+        head_index, rel_type, tail_index = head_index.to(device), rel_type.to(device), tail_index.to(device)
+        optimizer.zero_grad()
+
+        before = copy.deepcopy(model.state_dict())
+        loss, false_tail_index = model.loss(head_index, rel_type, tail_index)
+        loss.backward()
+        optimizer.step()
+        after = model.state_dict()
+
+        for key, params in after.items():
+            after[key] += lin_term(head_index, rel_type, tail_index, false_tail_index, ontology) * (after[key]-before[key])
+        model.load_state_dict(after)
+
+        total_loss += float(loss) * head_index.numel()
+        total_examples += head_index.numel()
+
+    return total_loss / total_examples
+
+@torch.no_grad()
+def test(data, model, 
+         batch_size,
+         device,
+         k = 10):
+    model.eval()
+    return model.test(
+        head_index=data.edge_index[0].to(device),
+        tail_index=data.edge_index[1].to(device),
+        rel_type=data.edge_attr.to(device),
+        batch_size=batch_size,
+        k=k, #The k in Hit@k
+    )
+
+def get_test_loss(loader, model, device):
+    model.eval()
+    total_loss = total_examples = 0
+    for head_index, rel_type, tail_index in loader:
+
+        head_index, rel_type, tail_index = head_index.to(device), rel_type.to(device), tail_index.to(device)
+        loss = model.loss(head_index, rel_type, tail_index)
+        total_loss += float(loss) * head_index.numel()
+        total_examples += head_index.numel()
+
+    return total_loss / total_examples
+
+
+def train_and_test_complex(
+                           model,
+                           train_data: torch_geometric.data.data.Data,
+                           test_data : torch_geometric.data.data.Data,
+                           hidden_channels:int = None,
+                           xp_name:str = '',
+                           epochs: int = 1000,
+                           batch_size: int = 4096,
+                           eval_period = 500,
+                           reset_parameters = False,
+                           params_save_path = '',
+                           use_wandb = False,
+                           device = 'cpu',
+                           dataset_name = 'iric'
+                           ):
+    
+    
+    # ----------------------------------------------- Reset parameters
+    if reset_parameters :
+        model.reset_parameters()
+
+    # ----------------------------------------------- Loader
+    loader = model.loader(
+                          head_index = train_data.edge_index[0],
+                          tail_index = train_data.edge_index[1],
+                          rel_type   = train_data.edge_attr,
+                          batch_size = batch_size,
+                          shuffle    = True)
+    test_loader = model.loader(
+                          head_index = test_data.edge_index[0],
+                          tail_index = test_data.edge_index[1],
+                          rel_type   = test_data.edge_attr,
+                          batch_size = batch_size,
+                          shuffle    = True)
+
+    # ----------------------------------------------- Optimizer
+    optimizer = optim.Adam(model.parameters())
+
+    # ----------------------------------------------- WandB
+    if use_wandb:
+        wandb.init(
+            settings=wandb.Settings(start_method="fork"),
+            project=xp_name,
+            
+            config={
+            "architecture": str(type(model)),
+            "dataset": dataset_name,
+            "epochs": epochs,
+            'hidden_channels' : hidden_channels,
+            'batch_size' : batch_size
+            }
+        )
+
+    # ----------------------------------------------- Train and eval
+    print('Train...')
+
+    torch.set_grad_enabled(True)
+    model.to(device)
+
+    train_losses = []
+    test_losses = []
+    for epoch in range(0, epochs+1):
+
+        loss = train(loader = loader,
+                     model  = model,
+                     optimizer = optimizer,
+                     device = device)
+        test_loss = get_test_loss(
+                            loader = test_loader,
+                            model  = model,
+                            device = device)
+        
+        train_losses.append(loss)
+        test_losses.append(test_loss)
+        
+
+        if use_wandb : 
+            wandb.log({"loss": loss, "loss on test": test_loss})
+        print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Loss on test : {test_loss:.4f}')
+
+    # ----------------------------------------------- Periodic Evaluation
+        if eval_period:
+            if epoch%eval_period == 0:
+                print('Test...')
+                rank, mrr, hits = test(test_data, model=model, device=device)
+
+                print(f'Epoch: {epoch:03d}, Val Mean Rank: {rank:.2f}',
+                    f'Val MRR: {mrr:.4f}, Val Hits@10: {hits:.4f}')
+                if use_wandb:
+                    wandb.log({"Val Mean Rank" : rank,
+                            "Val MRR" : mrr,
+                            "hits@10": hits})
+
+    # ----------------------------------------------- End WandB
+    if use_wandb:
+        print('End wandb...')
+        wandb.finish()
+        print("WandB finished.")
+
+    # ----------------------------------------------- Model to cpu
+    model.to('cpu')
+    
+    # ----------------------------------------------- Save model
+    if params_save_path != '':
+        torch.save(model.state_dict(), params_save_path)
+        print("Model saved at", params_save_path)
+
+    print("End")
+
+    return model, train_losses, train_losses
+
+
+def lin_sim_on_mapped_terms(mapped_term1, mapped_term2, map_to_GO:dict):
+    term1 = map_to_GO[mapped_term1]
+    term2 = map_to_GO[mapped_term2]
+    if (term1 in nxo.graph._node and term2 in nxo.graph._node):
+        sim = nxo.similarity(term1, term2).lin 
+        return sim
+    else:
+        return 0  
+
+
+def best_lim_sim_for_triple(head, rel, tail, mapped_alt_tails:dict, map_to_GO)-> torch.Tensor:
+    max_lin_sim=0
+    for alt_tail in mapped_alt_tails[(head, rel)]:
+
+        if (map_to_GO[tail] in nxo.graph._node
+            and
+            map_to_GO[alt_tail] in nxo.graph._node):
+                
+                sim = nxo.similarity(map_to_GO[tail], map_to_GO[alt_tail]).lin # Pourrait être amélioré : actuellement, on calcule plein de similarités différentes.
+                
+                if max_lin_sim < sim < 1:
+                    max_lin_sim = sim
+    
+    return max_lin_sim
+
+
+def best_lin_sims_for_batch(head_index:torch.Tensor, rel_type:torch.Tensor, tail_index:torch.Tensor):
+
+    batch = pd.DataFrame(torch.transpose(torch.stack((head_index,rel_type,tail_index)),
+                                         0,1)
+                        )
+    
+    return torch.Tensor(batch.apply(lambda row : best_lim_sim_for_triple(head=row[0],
+                                                                         rel=row[1],
+                                                                         tail=row[2]),
+                                    axis=1))
+
+def lin_sims_for_batch(term1: torch.Tensor, term2: torch.Tensor)->torch.Tensor:
+
+    batch = pd.DataFrame(torch.transpose(torch.stack((term1, term2)),
+                                         0,1)
+                        )    
+    return torch.Tensor(batch.apply(lambda row : best_lim_sim_for_triple(head=row[0],
+                                                                         rel=row[1],
+                                                                         tail=row[2]),
+                                    axis=1))
+
+def shuffle_tensor(t: torch.Tensor):
+    '''
+    Shuffles elements of a tensor.
+    WARNING :
+    shuffle_tensor(torch.tensor([[0,1,2,3,4,5],[6,7,8,9,0,1]])) returns :
+    tensor([[0, 1, 2, 3, 4, 5],  OR tensor([[6, 7, 8, 9, 0, 1],
+            [6, 7, 8, 9, 0, 1]])            [0, 1, 2, 3, 4, 5]])
+    '''
+    idx = torch.randperm(t.shape[0])
+    return t[idx].view(t.size())
+
+
+class tail_only_ComplEx(ComplEx):
+
+    '''
+    Overwritting random_sample() to make negative triples by setting a random tail to each triple,
+    instead of setting a random head or tail.
+    '''
+    @torch.no_grad()
+    def random_sample(
+        self,
+        head_index: torch.Tensor,
+        rel_type: torch.Tensor,
+        tail_index: torch.Tensor,
+        ):
+
+        """
+        Randomly samples negative triplets by replacing the tail.
+        Args:
+            head_index (torch.Tensor): The head indices.
+            rel_type (torch.Tensor): The relation type.
+            tail_index (torch.Tensor): The tail indices.
+        """
+
+        tail_index = shuffle_tensor(tail_index.clone())
+
+        return head_index, rel_type, tail_index
+    
+
+class best_LinSim_ComplEx(tail_only_ComplEx):
+  def loss(
+            self,
+            head_index: torch.Tensor,
+            rel_type: torch.Tensor,
+            tail_index: torch.Tensor,
+            ) -> torch.Tensor:
+            
+        '''
+        tail_only_ComplEx.loss() modified to account a LinSim term :
+        one withdraw the mean(bests similarities between each false tail of a triple to its possible tails) to the loss.
+        '''
+
+        pos = head_index, rel_type, tail_index
+
+        false_head_index, false_rel_type, false_tail_index = self.random_sample(head_index, rel_type, tail_index)
+        neg = false_head_index, false_rel_type, false_tail_index
+
+        pos_score = self(*pos)
+        neg_score = self(*neg)
+        scores = torch.cat([pos_score, neg_score], dim=0)
+
+        pos_target = torch.ones_like(pos_score) 
+        neg_target = torch.zeros_like(neg_score)
+        target = torch.cat([pos_target, neg_target], dim=0)
+
+        # Calculating LinSim(positive_head, negative_head) : 
+        similarities = best_lin_sims_for_batch(head_index, rel_type, false_tail_index)
+
+        return F.binary_cross_entropy_with_logits(scores, target) - torch.mean(similarities)
+
+class LinSim_ComplEx(tail_only_ComplEx):
+  def loss(
+            self,
+            head_index: torch.Tensor,
+            rel_type: torch.Tensor,
+            tail_index: torch.Tensor,
+            ) -> torch.Tensor:
+            
+        '''
+        tail_only_ComplEx.loss() modified to account a LinSim term : one simply withdraw mean(similarities(batch)) to the loss.
+        '''
+
+        pos = head_index, rel_type, tail_index
+
+        false_head_index, false_rel_type, false_tail_index = self.random_sample(head_index, rel_type, tail_index)
+        neg = false_head_index, false_rel_type, false_tail_index
+
+        pos_score = self(*pos)
+        neg_score = self(*neg)
+        scores = torch.cat([pos_score, neg_score], dim=0)
+
+        pos_target = torch.ones_like(pos_score) 
+        neg_target = torch.zeros_like(neg_score)
+        target = torch.cat([pos_target, neg_target], dim=0)
+
+        # stacking true and falses tails in df :
+        pos_and_neg_tails = pd.DataFrame(torch.stack((tail_index,false_tail_index)).transpose(0,1)).astype("int")
+
+        # Calculating LinSim(positive_head, negative_head) : 
+        similarities = torch.tensor(pos_and_neg_tails.apply(lambda row : lin_sim_on_mapped_terms(row[0], row[1]),
+                                                      axis = 1).values
+                                    )
+
+        return F.binary_cross_entropy_with_logits(scores, target) - torch.mean(similarities)
+
+class LinSim_Only_ComplEx(tail_only_ComplEx):
+  def loss(
+            self,
+            head_index: torch.Tensor,
+            rel_type: torch.Tensor,
+            tail_index: torch.Tensor,
+            ) -> torch.Tensor:
+            
+        '''
+        tail_only_ComplEx.loss() modified to account a LinSim term : one simply withdraw mean(similarities(batch)) to the loss.
+        '''
+
+        pos = head_index, rel_type, tail_index
+
+        false_head_index, false_rel_type, false_tail_index = self.random_sample(head_index, rel_type, tail_index)
+        neg = false_head_index, false_rel_type, false_tail_index
+
+        pos_score = self(*pos)
+        neg_score = self(*neg)
+        scores = torch.cat([pos_score, neg_score], dim=0)
+
+        pos_target = torch.ones_like(pos_score) 
+        neg_target = torch.zeros_like(neg_score)
+        target = torch.cat([pos_target, neg_target], dim=0)
+
+        # stacking true and falses tails in df :
+        pos_and_neg_tails = pd.DataFrame(torch.stack((tail_index,false_tail_index)).transpose(0,1)).astype("int")
+
+        # Calculating LinSim(positive_head, negative_head) : 
+        similarities = torch.tensor(pos_and_neg_tails.apply(lambda row : lin_sim_on_mapped_terms(row[0], row[1]),
+                                                      axis = 1).values
+                                    )
+
+        return torch.tensor([1])- torch.mean(similarities)
